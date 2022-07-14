67bd5dc44405bc42801225a9cdc47c57600178b7
msaari102 <mttsaari@gmail.com>
2022-07-14 20:25:39 +0300

    Fullstack open 2022 osa 13.6

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+require('express-async-errors')
+const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogsRouter = require('./controllers/blogs')
+// eslint-disable-next-line no-unused-vars
+const middleware = require('./utils/middleware')
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
